eb28132fd5414709b0fdb1e725b95d9ea0cdbe65
jasonni <niyunhuo@gmail.com>
2020-11-10 23:36:53 -0600

    添加了解释

--- 高频面试系列/pr169.java
+++ 高频面试系列/pr169.java
@@ -1,5 +1,8 @@
 /*
     使用Boyer-Moore Majority Vote Algorithm
+    用count来计算一个元素出现的次数，碰到的元素和统计的元素不想等时count减1。
+    如果count等于0，说明前面的元素出现的次数少于一半。因为majority是大于一半的，
+    最终会找到一个元素是majority
 */
 class Solution {
     public int majorityElement(int[] nums) {
